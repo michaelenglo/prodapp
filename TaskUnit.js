88ee41d47b73078e24701e52b101ce02b625fcbf
Michael Englo <menglo@optimalefficiency.net>
2018-06-16 06:21:47 -0700

    add swipeability to task unit

--- TaskUnit.js
+++ TaskUnit.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Divider } from 'react-native-elements';
-import { material, systemWeights } from 'react-native-typography';
+import { Divider, Icon } from 'react-native-elements';
+import { material, human, systemWeights } from 'react-native-typography';
+import Swipeout from 'react-native-swipeout';
 
 export default class TaskUnit extends React.Component {
 
   renderTaskUnit(key, task) {
 
     const isLeafTask = Object.keys(task.subtasks).length !== 0;
+    const checkButtonProps = {
+      backgroundColor: '#5de851',
+      component: <Icon name="check" color="#fff"/>,
+      underlayColor: '#fff',
+    };
 
     return (
-    <View key={key} style={styles.taskUnit}>
-      <Text style={[material.title, isLeafTask ? styles.opaque : []]}>{task.label}</Text>
-      {isLeafTask ? <Divider style={styles.divider} /> : null}
-      {isLeafTask ?
-        <View style={styles.taskBoard}>
-        {
-          Object.keys(task.subtasks).map(subtaskKey => {
-            return this.renderTaskUnit(subtaskKey, task.subtasks[subtaskKey]);
-          })
-        }
-        </View>
-      : null}
-    </View>
+      <View key={key} style={styles.taskUnit}>
+        <Swipeout 
+          left={[checkButtonProps]} 
+          backgroundColor={'rgba(0,0,0,0)'}
+        >
+          <Text style={isLeafTask ? material.caption : [human.title2, systemWeights.semibold]}>{task.label}</Text>
+          {isLeafTask ? <Divider style={styles.divider} /> : null}
+          {isLeafTask ?
+            <View style={styles.taskBoard}>
+            {
+              Object.keys(task.subtasks).map(subtaskKey => {
+                return this.renderTaskUnit(subtaskKey, task.subtasks[subtaskKey]);
+              })
+            }
+            </View>
+          : null}    
+        </Swipeout>
+      </View>
     )
   }
 
@@ -62,10 +73,10 @@ const styles = StyleSheet.create({
       paddingRight: 0,
     },
     divider: {
-      marginTop: 5,
+      marginTop: 1,
       marginBottom: 8,
     },
-    opaque: {
-      color: 'rgba(0, 0, 0, 0.5)',
-    }
+    disabledTaskColor: {
+      backgroundColor: '#dddddd',
+    },
 });
