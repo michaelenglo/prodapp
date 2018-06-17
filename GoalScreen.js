b64d3926523c70d8b3538c394a992b830d464c2f
Michael Englo <menglo@optimalefficiency.net>
2018-06-16 19:37:55 -0700

    resolve eslint

--- GoalScreen.js
+++ GoalScreen.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, ImageBackground } from 'react-native';
+import { StyleSheet, ScrollView, ImageBackground } from 'react-native';
 import TaskUnit from './TaskUnit';
 
 
 const task = {
-  label: "Throw a party",
+  label: 'Throw a party',
   subtasks: {
     task1ID: {
-      label: "Call friends",
+      label: 'Call friends',
       subtasks: {
         subtask1ID: {
-          label: "Call Lucy",
+          label: 'Call Lucy',
           subtasks: {},
         },
       },
     },
     task2ID: {
-      label: "Prepare for the party",
+      label: 'Prepare for the party',
       subtasks: {
         subtask2ID: {
-          label: "Buy food and drinks",
+          label: 'Buy food and drinks',
           subtasks: {},
         },
         subtask3ID: {
-          label: "Buy cups and plates",
+          label: 'Buy cups and plates',
           subtasks: {},
         },
       },
@@ -32,11 +32,18 @@ const task = {
 };
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state= {};
+  }
+
   render() {
     return (
       <ImageBackground source={require('./assets/dogbackground.png')} resizeMode="repeat" style={styles.woodBackground}>
         <ScrollView style={styles.container}>
-            <TaskUnit rootKey="maintask" task={task} />
+          <TaskUnit rootKey="maintask" task={task} />
         </ScrollView>
       </ImageBackground>
     );
--- TaskUnit.js
+++ TaskUnit.js
@@ -4,38 +4,37 @@ import { Divider, Icon } from 'react-native-elements';
 import { material, human, systemWeights } from 'react-native-typography';
 import Swipeout from 'react-native-swipeout';
 
-export default class TaskUnit extends React.Component {
+import background from './assets/blackbackground.png';
 
+class TaskUnit extends React.Component {
   renderTaskUnit(key, task) {
-
     const isLeafTask = Object.keys(task.subtasks).length !== 0;
     const checkButtonProps = {
       backgroundColor: '#5de851',
-      component: <Icon name="check" color="#fff"/>,
+      component: <Icon name="check" color="#fff" />,
       underlayColor: '#fff',
     };
 
     return (
       <View key={key} style={styles.taskUnit}>
-        <Swipeout 
-          left={[checkButtonProps]} 
-          backgroundColor={'rgba(0,0,0,0)'}
+        <Swipeout
+          left={[checkButtonProps]}
+          backgroundColor="rgba(0,0,0,0)"
           autoClose
         >
           <Text style={isLeafTask ? [material.caption, systemWeights.semibold] : [human.title2, systemWeights.regular]}>{task.label}</Text>
           {isLeafTask ? <Divider style={styles.divider} /> : null}
           {isLeafTask ?
-            <ImageBackground source={require('./assets/blackbackground.png')} resizeMode="repeat" style={styles.taskBoard}>
-            {
-              Object.keys(task.subtasks).map(subtaskKey => {
-                return this.renderTaskUnit(subtaskKey, task.subtasks[subtaskKey]);
-              })
+            <ImageBackground source={background} resizeMode="repeat" style={styles.taskBoard}>
+              {
+              Object.keys(task.subtasks).map(subtaskKey => this.renderTaskUnit(
+                subtaskKey, task.subtasks[subtaskKey]))
             }
             </ImageBackground>
-          : null}    
+          : null}
         </Swipeout>
       </View>
-    )
+    );
   }
 
   render() {
@@ -53,29 +52,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   taskUnit: {
-      width: '98%',
-      borderRadius: 4,
-      marginTop: 5,
-      backgroundColor: 'white',
-      padding: 5,
-      shadowOffset: {
-        height: 1,
-        width: 1,
-      },
-      shadowColor: 'black',
-      shadowOpacity: 0.8,
+    width: '98%',
+    borderRadius: 4,
+    marginTop: 5,
+    backgroundColor: 'white',
+    padding: 5,
+    shadowOffset: {
+      height: 1,
+      width: 1,
     },
-    taskBoard: {
-      borderRadius: 5,
-      paddingBottom: 5,
-      paddingLeft: 5,
-      paddingRight: 0,
-    },
-    divider: {
-      marginTop: 1,
-      marginBottom: 8,
-    },
-    disabledTaskColor: {
-      backgroundColor: '#dddddd',
-    },
+    shadowColor: 'black',
+    shadowOpacity: 0.8,
+  },
+  taskBoard: {
+    borderRadius: 5,
+    paddingBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 0,
+  },
+  divider: {
+    marginTop: 1,
+    marginBottom: 8,
+  },
+  disabledTaskColor: {
+    backgroundColor: '#dddddd',
+  },
 });
+
+export default TaskUnit;
